91246e280e1bcc4ae4a06cc39c34b0c947a0f0b6
hajnrych.jan@gmail.com <hajnrych.jan@gmail.com>
2021-06-04 15:32:24 +0200

    modify pin id names for readability

--- read_mic.c
+++ read_mic.c
@@ -1,5 +1,5 @@
-int red = 7;
-const int micPin  = A0;
+int diode_pin_id = 7;
+const int microphone_pin_id = A0;
 int avg = 0;
 const byte N = 16;
 int values[N];
@@ -7,8 +7,8 @@ int delay_time_ms = 1000/120;
 unsigned long time;
 
 void setup() {
-  pinMode(red, OUTPUT);
-  pinMode(micPin, INPUT);
+  pinMode(diode_pin_id, OUTPUT);
+  pinMode(microphone_pin_id, INPUT);
   Serial.println(F("Initialize System"));
   Serial.begin (9600);
     for (byte i=0; i<N; i++)
@@ -18,15 +18,15 @@ void setup() {
 void readMicrophone( ) { /* function readMicrophone */
   for (byte i=0; i<N-1; i++)
     values[i+1] = values[i];
-  values[0] = analogRead(micPin);
+  values[0] = analogRead(microphone_pin_id);
   avg = 0;
   for (byte i=0; i<N; i++)
     avg+=values[i];
   avg = avg/N;  
   if (values[0] - avg > 10)
-    digitalWrite(red, HIGH);
+    digitalWrite(diode_pin_id, HIGH);
   else
-    digitalWrite(red, LOW);
+    digitalWrite(diode_pin_id, LOW);
   time = millis();
   
   Serial.print(time);
@@ -36,5 +36,5 @@ void readMicrophone( ) { /* function readMicrophone */
 
 void loop() {
   readMicrophone();
-  //delay(delay_time_ms);
+  delay(delay_time_ms);
 }
